c46223b6746169fdfd3009e7cc06747627fd154b
love-cong <2404662491@qq.com>
2019-09-18 21:17:40 +0800

    页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/home/index.vue' // 路由级组件
+// import Home from './views/home/index.vue' // 路由级组件  完整写法
 import Home from './views/home/'
+import Login from './views/login'
 
 Vue.use(Router)
 
 export default new Router({
   // 一级路由
   routes: [{
-    path: '/',
+    path: '/', // 强制推到home下
+    redirect: '/home'
+
+  },
+  // 首页路由
+  {
+    path: '/home',
     component: Home
+  },
+  // 登录路由
+  {
+    path: '/login',
+    component: Login
   }
     // {
     //   path: '/about',
