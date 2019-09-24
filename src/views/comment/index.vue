47d38d372fd6c244bb405db7114fb88dc9c908af
love-cong <2404662491@qq.com>
2019-09-24 21:42:36 +0800

    评论列表-评论数据加载到页面及状态过滤2

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -10,10 +10,14 @@
         <el-table :data="list">
           <!-- el-table-column 列  属性prop 键名 =>字段名 lable表头 =>-->
           <el-table-column width="600px" label="标题" prop="title"></el-table-column>
-          <el-table-column align="center" label="评论状态" prop="comment_status"></el-table-column>
+          <el-table-column :formatter="stateFormatter" align="center" label="评论状态" prop="comment_status"></el-table-column>
           <el-table-column align="center" label="总评论数" prop="total_comment_count"></el-table-column>
           <el-table-column align="center" label="粉丝评论数" prop="fans_comment_count"></el-table-column>
-          <el-table-column align="center" label="操作" ></el-table-column>
+          <el-table-column align="center" label="操作" >
+            <!-- 自定义内容 获取其他列的数据 -->
+            <el-button size="small" type="text">修改</el-button>
+            <el-button size="small" type="text">关闭评论</el-button>
+          </el-table-column>
         </el-table>
   </el-card>
 </template>
@@ -34,6 +38,10 @@ export default {
       }).then(result => {
         this.list = result.data.results // 把返回的数据赋值给list
       })
+    },
+    // filter =>return
+    stateFormatter (row, colum, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
     }
   },
   // 钩子函数
