6c4c23efea081ddc29fddeea4382a7c84f46edf7
love-cong <2404662491@qq.com>
2019-09-25 15:52:50 +0800

    评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -26,6 +26,19 @@
 
           </el-table-column>
         </el-table>
+        <!-- 分页 -->
+        <el-row type="flex" justify="center" style="margin:20px 0">
+          <!-- page-size 是每页多少条 -->
+          <el-pagination
+            @current-change="changePage"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            background
+            layout="prev, pager, next"
+            :total="page.total">
+          </el-pagination>
+        </el-row>
+
   </el-card>
 </template>
 
@@ -33,17 +46,29 @@
 export default {
   data () {
     return {
-      list: [{}]
+      list: [],
+      // 专门存放分页信息
+      page: {
+        total: 0, // 总条数
+        currentPage: 1, // 默认第一页
+        pageSize: 10
+      }
+
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage // 更新最新的页码给currentPage
+      this.getComment()
+    },
     // 获取评论
     getComment () {
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' } // params 是路径参数也就是query参数
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize } // params 是路径参数也就是query参数
       }).then(result => {
         this.list = result.data.results // 把返回的数据赋值给list
+        this.page.total = result.data.total_count // 把总条数给分页组件的总条数
       })
     },
     // filter =>return
@@ -68,7 +93,7 @@ export default {
   },
   // 钩子函数
   created () {
-    this.getComment()
+    this.getComment()// 获取第一页 每页十条数据
   }
 }
 // table使用和过滤提交反了
