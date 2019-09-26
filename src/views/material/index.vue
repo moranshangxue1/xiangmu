eea731e4a25aa6ca0ec9625db1ca4f9a38dbb46f
love-cong <2404662491@qq.com>
2019-09-26 20:51:54 +0800

    素材列表-数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,21 +1,93 @@
 <template>
   <el-card>
       <!-- header具名 是给卡片的具名 -->
-      <bread-crumb slot="header">
+        <bread-crumb slot="header">
       <!-- title具名 是面包屑组件的具名 -->
           <template slot="title">
               素材管理
           </template>
-      </bread-crumb>
+        </bread-crumb>
+        <el-tabs v-model="activeName" @tab-click="getMaterial">
+            <el-tab-pane label="全部素材" name="all">
+                <div class="img-list">
+                    <el-card class="img-item" v-for="item in list" :key="item.id">
+                        <img :src="item.url" alt="">
+                        <div class="operate">
+                            <i :style='{color: item.is_collected ? "red" : "#000"}' class="el-icon-star-on"></i>
+                            <i class="el-icon-delete-solid"></i>
+                        </div>
+                    </el-card>
+                </div>
+
+            </el-tab-pane>
+            <el-tab-pane label="收藏素材" name="collect">
+                <div class="img-list">
+                    <el-card class="img-item" v-for="item in list" :key="item.id">
+                        <img :src="item.url" alt="">
+                    </el-card>
+                </div>
+            </el-tab-pane>
+        </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all', // 默认选中全部
+      list: [] // 定义list接收数据
+    }
+  },
+  methods: {
+    //   获取素材列表
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        // this.activeName === 'collect' 相当于去找隐藏的数据
+        // 如果不等于collect 相当于找全部的数据
+        params: { collect: this.activeName === 'collect' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+   .img-list {
+    //    display: inline;
+       display: flex;
+       flex-wrap: wrap;
+       justify-content: center;
 
+       .img-item {
+           width: 180px;
+           height: 180px;
+           border-radius: 6px;
+           margin: 20px;
+           position: relative;
+           img {
+               width: 100%;
+           }
+           .operate {
+               background-color: #f4f5f6;
+               position: absolute;
+               left: 0;
+               bottom: 0;
+               width: 100%;
+               height: 30px;
+               display: flex;
+               justify-content: space-around;
+               align-items: center;
+              i {
+                   font-size: 18px;
+              }
+           }
+       }
+   }
 </style>
